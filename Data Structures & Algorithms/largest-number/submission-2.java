da5d18ce91c55237790afd9e08250eb91d9aab9b
Moyer0 <136533700+Moyer0@users.noreply.github.com>
2026-04-02 13:13:06 -0600

    Add: largest-number - submission-2

---
+++ Data Structures & Algorithms/largest-number/submission-2.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+        // 1) Convert to strings
+        String[] s = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            s[i] = String.valueOf(nums[i]);
+        }
+
+        // 2) Sort with custom comparator: ab vs ba
+        Arrays.sort(s, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                String ab = a + b;
+                String ba = b + a;
+                // We want descending order
+                return ba.compareTo(ab);
+            }
+        });
+
+        // 3) If the largest is "0", the entire number is 0
+        if (s.length > 0 && s[0].equals("0")) {
+            return "0";
+        }
+
+        // 4) Join all parts
+        StringBuilder sb = new StringBuilder();
+        for (String part : s) {
+            sb.append(part);
+        }
+        return sb.toString();
+    }
+}
